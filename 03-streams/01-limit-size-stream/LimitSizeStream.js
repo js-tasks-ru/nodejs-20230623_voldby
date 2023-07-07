a4d13340dee2769e4b80d6d1e2d64b641a32e235
Vladimir Vernikovsky <voldby@gmail.com>
2023-07-07 19:19:34 +0300

    stream tasks 1,2,3,5

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,22 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.transp_bytes = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    this.transp_bytes += chunk.length;
 
+    if (this.transp_bytes > this.limit)
+    {
+      this.transp_bytes -= chunk.length;
+      callback(new LimitExceededError());
+    }
+    else
+    {
+      callback(null, chunk);
+    }
   }
 }
 
--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,33 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.last_line = '';
+    this.iEOL = 0;
+    this.liEOL = 0;
   }
 
   _transform(chunk, encoding, callback) {
+     let strC = chunk.toString();
+
+     this.iEOL = 0;
+     this.liEOL = 0;
+     while ((this.iEOL = strC.indexOf(os.EOL, this.liEOL)) >= 0)
+     {
+       this.last_line += strC.slice(this.liEOL, this.iEOL);
+       this.push(this.last_line);
+       this.last_line = '';
+         this.liEOL = this.iEOL+os.EOL.length
+     }
+
+     if (this.liEOL < strC.length) {
+         this.last_line += strC.slice(this.liEOL);
+     }
+
+     callback();
   }
 
   _flush(callback) {
+    callback(null, this.last_line);
   }
 }
 
