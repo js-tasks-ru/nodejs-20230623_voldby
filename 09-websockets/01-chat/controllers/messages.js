8f1989116c92c9356484ddcb2730ecc0d0f45f05
Vladimir Vernikovsky <voldby@gmail.com>
2023-08-11 16:52:43 +0300

    09-websockets/01-chat

--- 09-websockets/01-chat/controllers/messages.js
+++ 09-websockets/01-chat/controllers/messages.js
@@ -1,5 +1,18 @@
 const Message = require('../models/Message');
+const MessageMap = require('../mappers/message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {};
+
+  let messages = await Message.find({chat: ctx.user.id}, null, { sort: { date: -1 }, limit: 20 }).exec();
+  let res = [];
+  if (messages) {
+    for (const msg of messages) {
+      res.push(MessageMap(msg));
+    }
+
+    ctx.body = {messages: res};
+  }
+  else {
+    ctx.body = {};
+  }
 };
